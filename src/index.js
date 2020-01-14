1367f167b9fa6ff31271abde61a79f9e6eea3d90
Julio <juliodavidchopite@gmail.com>
2020-01-13 23:27:33 -0300

    entended our index to add our store a middleware with with thunk

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import { anecdotesReducer } from './reducers/anecdotesReducer'
 import { notificationReducer } from './reducers/notificationReducer'
+import thunk from 'redux-thunk'
 
 const reducer = combineReducers({
   anecdotes: anecdotesReducer,
   notification: notificationReducer
 })
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>

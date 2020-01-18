f66140a8df3f5d99cffeadea6632f03d299d58e0
Julio <juliodavidchopite@gmail.com>
2020-01-18 00:27:25 -0300

    made our menu properly display the loging link when the user hasnt logged in and display the user with the logout button when the user still has a session open

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,18 +1,45 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { actionCreatorSetUsername as setUser } from '../reducers/userReducer'
+import { connect } from 'react-redux'
 
-const Menu = () => {
+const Menu = (props) => {
   const padding = {
     paddingRight: 5
   }
+
+  const loggedUser = () => {
+    return(
+      <div>
+        {props.user} Logged In
+        <button onClick={()=>{
+           window.localStorage.clear()
+           props.setUser(null)
+        }}>Logout</button>
+      </div>
+    )
+  }
+
   return (
     <div>
       <Link style={padding} to='/anecdotes' >anecdotes</Link>
       <Link style={padding} to='/create' >create new</Link>
       <Link style={padding} to='/about' >about</Link>
-      <Link style={padding} to='/login' >login</Link>
+      {
+        props.user
+        ? loggedUser(props.user)
+        : <Link style={padding} to='/login' >login</Link>
+      }
     </div>
   )
 }
 
-export default Menu
+const mapStateToProps = state => ({
+  user: state.user
+})
+
+const mapDispatchToProps = {
+  setUser
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Menu)

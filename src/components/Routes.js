ffa6e5e4dbf1e33243abbd40881fb838b49362c3
Julio <juliodavidchopite@gmail.com>
2020-01-19 07:56:10 -0300

    added a route to render the register component and modified how some components are imported

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -3,8 +3,9 @@ import { Route } from 'react-router-dom'
 import AnecdoteList from '../components/AnecdoteList'
 import CreateNewAnecdote from '../components/CreateNewAnecdote'
 import Anecdote from '../components/Anecdote'
-import About from '../components/About'
-import Login from '../components/Login'
+import About from './About'
+import Login from './Login'
+import Register from './Register'
 
 const Routes = () => {
   return (
@@ -15,6 +16,7 @@ const Routes = () => {
       <Route path="/about" render={() => <About /> } />
       <Route exact path='/anecdotes/:id' render={({ match }) => <Anecdote id={match.params.id}/>}  />
       <Route path="/login" render={() => <Login /> } />
+      <Route path="/register" render={() => <Register /> } />
     </div>
   )
 }

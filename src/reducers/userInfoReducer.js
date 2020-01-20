e01facbc2527f67cd8f3314bead47f89eefc4165
Julio <juliodavidchopite@gmail.com>
2020-01-20 03:58:40 -0300

    added a new reducer for userinfo with its action creator to allow our components to use this state as props

---
+++ src/reducers/userInfoReducer.js
@@ -0,0 +1,20 @@
+const userInfoReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_USER_INFO':
+      return action.data
+  default : 
+  return state
+  }
+}
+
+const actionCreatorSetUserInfo = (data) => {
+  return {
+    type: 'SET_USER_INFO',
+    data
+  }
+}
+
+export {
+  userInfoReducer,
+  actionCreatorSetUserInfo
+}
